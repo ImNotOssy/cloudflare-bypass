c9a787b8b344e8dfead471802481990711d26fe9
ImNotOssy <85385882+ImNotOssy@users.noreply.github.com>
2024-09-20 14:58:15 -0500

    Update base_detector.py

--- cloudflare_bypass/base_detector.py
+++ cloudflare_bypass/base_detector.py
@@ -1,33 +1,16 @@
 from typing import Union, Tuple
 import cv2
-from PIL import ImageGrab
+from PIL import Image
 import numpy as np
-
+from io import BytesIO
 
 class BaseDetector:
     def __init__(self, template_path: str, threshold: float = 0.6) -> None:
-        """
-        Initializes the BaseDetector with a template image and a matching threshold.
-
-        Parameters:
-            - template_path (str): Path to the template image file.
-            - threshold (float): Matching threshold (default is 0.8).
-        """
         self.template = cv2.imread(template_path, 0)
         self.threshold = threshold
         self.matched_bbox = None
 
     def _match(self, img: np.ndarray, template: np.ndarray) -> Union[None, Tuple[int]]:
-        """
-        Performs template matching on the input image.
-
-        Parameters:
-            - img (np.ndarray): Input image.
-            - template (np.ndarray): Template image.
-
-        Returns:
-            - Union[None, Tuple[int]]: Bounding box coordinates if match is found, else None.
-        """
         result = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
         _, confidence, _, max_loc = cv2.minMaxLoc(result)
 
@@ -39,15 +22,11 @@ class BaseDetector:
         else:
             return None
 
-    def is_detected(self) -> bool:
-        """
-        Checks if the template is detected in the current screen.
-
-        Returns:
-            - bool: True if template is detected, else False.
-        """
-        img = np.array(ImageGrab.grab())
-        img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    def is_detected(self, driver) -> bool:
+        screenshot = driver.get_screenshot_as_png()
+        image = Image.open(BytesIO(screenshot))
+        img_np = np.array(image)
+        img_gray = cv2.cvtColor(img_np, cv2.COLOR_RGB2GRAY)
 
         self.matched_bbox = self._match(img_gray, self.template)
         return self.matched_bbox is not None

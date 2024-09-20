1eb52343fb599f0aefa4e7797dac36ba07b6f5cd
ImNotOssy <85385882+ImNotOssy@users.noreply.github.com>
2024-09-20 16:55:46 -0500

    Update cloudflare_detector.py

--- cloudflare_bypass/cloudflare_detector.py
+++ cloudflare_bypass/cloudflare_detector.py
@@ -1,35 +1,27 @@
 from pathlib import Path
+import sys
 from cloudflare_bypass.base_detector import BaseDetector
 
-image_dir = Path(__file__).parent
+if hasattr(sys, '_MEIPASS'):
+    image_dir = Path(sys._MEIPASS) / 'cloudflare_bypass' / 'images'
+else:
+    image_dir = Path(__file__).parent / 'images'
 
 
 class CloudFlarePopupDetector(BaseDetector):
     def __init__(self, mode: str = 'light'):
-        """
-        Initializes the CloudFlarePopupDetector with the path to the template image.
-
-        The template image is used for detecting the presence of a CloudFlare popup.
-
-        """
         if mode == 'light':
-            self.template_path = str(image_dir / 'images/cf_popup.png')
+            self.template_path = str(image_dir / 'cf_popup.png')
         else:
-            self.template_path = str(image_dir / 'images/cf_popup_dark.png')
+            self.template_path = str(image_dir / 'cf_popup_dark.png')
 
         super().__init__(template_path=self.template_path)
 
 
 class CloudFlareLogoDetector(BaseDetector):
     def __init__(self, mode: str = 'light'):
-        """
-        Initializes the CloudFlareLogoDetector with the path to the template image.
-
-        The template image is used for detecting the presence of the CloudFlare logo.
-
-        """
         if mode == 'light':
-            self.template_path = str(image_dir / 'images/cf_logo.png')
+            self.template_path = str(image_dir / 'cf_logo.png')
         else:
-            self.template_path = str(image_dir / 'images/cf_logo_dark.png')
+            self.template_path = str(image_dir / 'cf_logo_dark.png')
         super().__init__(template_path=self.template_path)
